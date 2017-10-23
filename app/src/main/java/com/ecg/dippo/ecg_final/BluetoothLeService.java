64e6d983cd4b4c598248c37c87f377bd0f8c6b73
Gabriel De Felipe <gabriel.felipe@frixtel.com>
2017-10-23 16:24:03 -0300

    fixes

--- app/src/main/java/com/ecg/dippo/ecg_final/BluetoothLeService.java
+++ app/src/main/java/com/ecg/dippo/ecg_final/BluetoothLeService.java
@@ -124,24 +124,7 @@ public class BluetoothLeService extends Service {
                                  final BluetoothGattCharacteristic characteristic) {
         final Intent intent = new Intent(action);
 
-        // This is special handling for the Heart Rate Measurement profile.  Data parsing is
-        // carried out as per profile specifications:
-        // http://developer.bluetooth.org/gatt/characteristics/Pages/CharacteristicViewer.aspx?u=org.bluetooth.characteristic.heart_rate_measurement.xml
-        if (UUID_HEART_RATE_MEASUREMENT.equals(characteristic.getUuid())) {
-            int flag = characteristic.getProperties();
-            int format = -1;
-            if ((flag & 0x01) != 0) {
-                format = BluetoothGattCharacteristic.FORMAT_UINT16;
-                Log.d(TAG, "Heart rate format UINT16.");
-            } else {
-                format = BluetoothGattCharacteristic.FORMAT_UINT8;
-                Log.d(TAG, "Heart rate format UINT8.");
-            }
-            final int heartRate = characteristic.getIntValue(format, 1);
-            Log.d(TAG, String.format("Received heart rate: %d", heartRate));
-            intent.putExtra(EXTRA_DATA, String.valueOf(heartRate));
-        } else {
-            // For all other profiles, writes the data formatted in HEX.
+            // ACA LLEGA LA DATA , HAGO UN PRIMERO PROCESOS PARA LEER QUE LLEGA Y LA MANDO AL INTENT QUE LA NECESITA
 
             final byte[] data = characteristic.getValue();
 
@@ -154,7 +137,7 @@ public class BluetoothLeService extends Service {
                     stringBuilder.append(String.format("%02X ", byteChar));
                 intent.putExtra(EXTRA_DATA, new String(data) + "\n" + stringBuilder.toString());
             }
-        }
+
         sendBroadcast(intent);
     }
 
