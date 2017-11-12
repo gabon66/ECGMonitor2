73534fba35de61f08a9bd1f8904220e492aa36a9
Gabriel De Felipe <gabriel.felipe@frixtel.com>
2017-11-12 01:16:26 -0300

    fixes

--- app/src/main/java/com/ecg/dippo/ecg_final/Demo1Activity.java
+++ app/src/main/java/com/ecg/dippo/ecg_final/Demo1Activity.java
@@ -25,6 +25,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.ecg.dippo.ecg_final.data.DataManager;
 import com.ihealth.communication.control.Po3Control;
 import com.ihealth.communication.control.PoProfile;
 import com.ihealth.communication.manager.iHealthDevicesCallback;
@@ -34,6 +35,20 @@ import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 import com.polidea.rxandroidble.RxBleClient;
 import com.polidea.rxandroidble.RxBleDevice;
+import com.scichart.charting.model.dataSeries.IXyDataSeries;
+import com.scichart.charting.modifiers.ModifierGroup;
+import com.scichart.charting.visuals.SciChartSurface;
+import com.scichart.charting.visuals.annotations.HorizontalAnchorPoint;
+import com.scichart.charting.visuals.annotations.TextAnnotation;
+import com.scichart.charting.visuals.annotations.VerticalAnchorPoint;
+import com.scichart.charting.visuals.axes.AutoRange;
+import com.scichart.charting.visuals.axes.IAxis;
+import com.scichart.charting.visuals.renderableSeries.IRenderableSeries;
+import com.scichart.core.framework.UpdateSuspender;
+import com.scichart.data.model.DoubleRange;
+import com.scichart.data.model.ISciList;
+import com.scichart.drawing.utility.ColorUtil;
+import com.scichart.extensions.builders.SciChartBuilder;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -41,11 +56,16 @@ import org.json.JSONObject;
 import org.json.JSONTokener;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.UUID;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 import rx.Subscription;
 
@@ -57,6 +77,23 @@ public class Demo1Activity extends AppCompatActivity {
     //String macAddress = "42:AC:0B:83:EB:B0";
 
     String macPusli="34B1F7CD4B2E";
+
+
+    IXyDataSeries<Double, Double> series0 ;
+    IXyDataSeries<Double, Double> series1 ;
+    private final static long TIME_INTERVAL = 20;
+
+    private double[] sourceData;
+
+    private int _currentIndex=0;
+    private int _totalIndex=0;
+
+    private chartEcg.TraceAOrB whichTrace = chartEcg.TraceAOrB.TraceA;
+
+    private final ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+    private ScheduledFuture<?> schedule;
+
+    private volatile boolean isRunning = true;
 
 
     private final Handler mHandlerGraph = new Handler();
@@ -153,6 +190,72 @@ public class Demo1Activity extends AppCompatActivity {
                 (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
         mBluetoothAdapter = bluetoothManager.getAdapter();
 
+
+        final SciChartSurface surface = new SciChartSurface(this);
+        LinearLayout chartLayout = (LinearLayout) findViewById(R.id.chartecg);
+        chartLayout.addView(surface);
+
+        SciChartBuilder.init(this);
+
+
+        // Obtain the SciChartBuilder instance
+        final SciChartBuilder sciChartBuilder = SciChartBuilder.instance();
+
+        sourceData = DataManager.getInstance().loadWaveformData(getApplicationContext());
+
+
+
+        series0 = sciChartBuilder.newXyDataSeries(Double.class, Double.class).withFifoCapacity(3850).build();
+        series1 = sciChartBuilder.newXyDataSeries(Double.class, Double.class).withFifoCapacity(3850).build();
+
+        final IAxis xBottomAxis = sciChartBuilder.newNumericAxis()
+                //.withVisibleRange(new DoubleRange(0d, 10d))
+                .withVisibility(1)
+                .withAutoRangeMode(AutoRange.Never)
+                //.withAxisTitle("Time (seconds)")
+                .build();
+
+        final IAxis yRightAxis = sciChartBuilder.newNumericAxis()
+                .withVisibleRange(new DoubleRange(-0.5d, 1.5d))
+                .withAxisTitle("Voltaje (mV)")
+                .build();
+
+        final IRenderableSeries rs1 = sciChartBuilder.newLineSeries()
+                .withDataSeries(series0)
+                .build();
+
+        final IRenderableSeries rs2 = sciChartBuilder.newLineSeries()
+                .withDataSeries(series1)
+                .build();
+
+        Collections.addAll(surface.getXAxes(), xBottomAxis);
+        Collections.addAll(surface.getYAxes(), yRightAxis);
+        Collections.addAll(surface.getRenderableSeries(), rs1, rs2);
+
+
+
+
+        final ISciList<Double> xValues0 = series0.getXValues();
+        final ISciList<Double> yValues0 = series0.getYValues();
+        final ISciList<Double> xValues1 = series1.getXValues();
+        final ISciList<Double> yValues1 = series1.getYValues();
+        series0.append(xValues0, yValues0);
+        series1.append(xValues1, yValues1);
+
+        schedule = scheduledExecutorService.scheduleWithFixedDelay(new Runnable() {
+            @Override
+            public void run() {
+                UpdateSuspender.using(surface, appendDataRunnable);
+            }
+        }, 0, TIME_INTERVAL, TimeUnit.MILLISECONDS);
+
+
+
+
+
+
+
+
         lblpR=(TextView)findViewById(R.id.txtPR);
         lblSPO2=(TextView)findViewById(R.id.txtSPO2);
         dataPR=(LinearLayout)findViewById(R.id.dataPr);
@@ -205,7 +308,6 @@ public class Demo1Activity extends AppCompatActivity {
 
         iHealthDevicesManager.getInstance().addCallbackFilterForDeviceType(callbackId,
                 iHealthDevicesManager.TYPE_PO3);
-
 
     }
 
@@ -296,7 +398,7 @@ public class Demo1Activity extends AppCompatActivity {
                 mHandlerGraph.postDelayed(this, 200);
             }
         };
-        mHandlerGraph.postDelayed(mTimer2, 1000);
+       // mHandlerGraph.postDelayed(mTimer2, 1000);
     }
 
     private void updateConnectionState(final int resourceId) {
@@ -621,4 +723,55 @@ public class Demo1Activity extends AppCompatActivity {
     private double getRandom() {
         return mLastRandom += mRand.nextDouble()*0.5 - 0.25;
     }
+
+
+
+
+    //  EGG CHART ------------
+
+
+
+
+    private synchronized void appendPoint(double sampleRate) {
+        if (_currentIndex >= sourceData.length) {
+            _currentIndex = 0;
+        }
+
+        // Get the next voltage and time, and append to the chart
+        double voltage = sourceData[_currentIndex];
+        double time = (_totalIndex / sampleRate) % 10;
+
+        if (whichTrace == chartEcg.TraceAOrB.TraceA) {
+            series0.append(time, voltage);
+            series1.append(time, Double.NaN);
+        } else {
+            series0.append(time, Double.NaN);
+            series1.append(time, voltage);
+        }
+
+        _currentIndex++;
+        _totalIndex++;
+
+        if (_totalIndex % 4000 == 0) {
+            whichTrace = whichTrace == chartEcg.TraceAOrB.TraceA ? chartEcg.TraceAOrB.TraceB : chartEcg.TraceAOrB.TraceA;
+        }
+    }
+
+    private final Runnable appendDataRunnable = new Runnable() {
+        @Override
+        public synchronized void run() {
+            if (isRunning) {
+                for (int i = 0; i < 10; i++) {
+                    appendPoint(400);
+                }
+            }
+        }
+    };
+
+    enum TraceAOrB {
+        TraceA,
+        TraceB
+    }
+
+
 }
---
+++ app/src/main/java/com/ecg/dippo/ecg_final/chartEcg.java
@@ -0,0 +1,161 @@
+package com.ecg.dippo.ecg_final;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.ecg.dippo.ecg_final.data.DataManager;
+import com.scichart.charting.model.dataSeries.IXyDataSeries;
+import com.scichart.charting.model.dataSeries.XyDataSeries;
+import com.scichart.charting.modifiers.ModifierGroup;
+import com.scichart.charting.visuals.SciChartSurface;
+import com.scichart.charting.visuals.annotations.HorizontalAnchorPoint;
+import com.scichart.charting.visuals.annotations.TextAnnotation;
+import com.scichart.charting.visuals.annotations.VerticalAnchorPoint;
+import com.scichart.charting.visuals.axes.AutoRange;
+import com.scichart.charting.visuals.axes.IAxis;
+import com.scichart.charting.visuals.pointmarkers.EllipsePointMarker;
+import com.scichart.charting.visuals.renderableSeries.IRenderableSeries;
+import com.scichart.core.framework.UpdateSuspender;
+import com.scichart.data.model.DoubleRange;
+import com.scichart.data.model.ISciList;
+import com.scichart.drawing.utility.ColorUtil;
+import com.scichart.extensions.builders.SciChartBuilder;
+
+import java.util.Collections;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+public class chartEcg extends AppCompatActivity {
+
+
+    IXyDataSeries<Double, Double> series0 ;
+    IXyDataSeries<Double, Double> series1 ;
+    private final static long TIME_INTERVAL = 20;
+
+    private double[] sourceData;
+
+    private int _currentIndex=0;
+    private int _totalIndex=0;
+
+    private TraceAOrB whichTrace = TraceAOrB.TraceA;
+
+    private final ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+    private ScheduledFuture<?> schedule;
+
+    private volatile boolean isRunning = true;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chart_ecg);
+
+
+
+        final SciChartSurface surface = new SciChartSurface(this);
+        LinearLayout chartLayout = (LinearLayout) findViewById(R.id.chartecg2);
+
+
+        SciChartBuilder.init(this);
+
+        // Obtain the SciChartBuilder instance
+        final SciChartBuilder sciChartBuilder = SciChartBuilder.instance();
+
+        sourceData = DataManager.getInstance().loadWaveformData(getApplicationContext());
+
+
+
+        series0 = sciChartBuilder.newXyDataSeries(Double.class, Double.class).withFifoCapacity(3850).build();
+        series1 = sciChartBuilder.newXyDataSeries(Double.class, Double.class).withFifoCapacity(3850).build();
+
+        final IAxis xBottomAxis = sciChartBuilder.newNumericAxis()
+                .withVisibleRange(new DoubleRange(0d, 10d))
+                .withAutoRangeMode(AutoRange.Never)
+                .withAxisTitle("Time (seconds)")
+                .build();
+
+        final IAxis yRightAxis = sciChartBuilder.newNumericAxis()
+                .withVisibleRange(new DoubleRange(-0.5d, 1.5d))
+                .withAxisTitle("Voltage (mV)")
+                .build();
+
+        final IRenderableSeries rs1 = sciChartBuilder.newLineSeries()
+                .withDataSeries(series0)
+                .build();
+
+        final IRenderableSeries rs2 = sciChartBuilder.newLineSeries()
+                .withDataSeries(series1)
+                .build();
+
+        Collections.addAll(surface.getXAxes(), xBottomAxis);
+        Collections.addAll(surface.getYAxes(), yRightAxis);
+        Collections.addAll(surface.getRenderableSeries(), rs1, rs2);
+
+
+
+
+        final ISciList<Double> xValues0 = series0.getXValues();
+        final ISciList<Double> yValues0 = series0.getYValues();
+        final ISciList<Double> xValues1 = series1.getXValues();
+        final ISciList<Double> yValues1 = series1.getYValues();
+        series0.append(xValues0, yValues0);
+        series1.append(xValues1, yValues1);
+
+        schedule = scheduledExecutorService.scheduleWithFixedDelay(new Runnable() {
+            @Override
+            public void run() {
+                UpdateSuspender.using(surface, appendDataRunnable);
+            }
+        }, 0, TIME_INTERVAL, TimeUnit.MILLISECONDS);
+
+        chartLayout.addView(surface);
+    }
+
+
+
+
+
+    private synchronized void appendPoint(double sampleRate) {
+        if (_currentIndex >= sourceData.length) {
+            _currentIndex = 0;
+        }
+
+        // Get the next voltage and time, and append to the chart
+        double voltage = sourceData[_currentIndex];
+        double time = (_totalIndex / sampleRate) % 10;
+
+        if (whichTrace == TraceAOrB.TraceA) {
+            series0.append(time, voltage);
+            series1.append(time, Double.NaN);
+        } else {
+            series0.append(time, Double.NaN);
+            series1.append(time, voltage);
+        }
+
+        _currentIndex++;
+        _totalIndex++;
+
+        if (_totalIndex % 4000 == 0) {
+            whichTrace = whichTrace == TraceAOrB.TraceA ? TraceAOrB.TraceB : TraceAOrB.TraceA;
+        }
+    }
+
+    private final Runnable appendDataRunnable = new Runnable() {
+        @Override
+        public synchronized void run() {
+            if (isRunning) {
+                for (int i = 0; i < 10; i++) {
+                    appendPoint(400);
+                }
+            }
+        }
+    };
+
+    enum TraceAOrB {
+        TraceA,
+        TraceB
+    }
+}
